a5aca29403d8e33d6208d74b63e261cbb6833450
Cody Epstein <118944207+kepsteen@users.noreply.github.com>
2024-08-22 10:19:01 -0700

    Time: 54 ms (41.47%), Space: 48.9 MB (55.53%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -5,7 +5,6 @@
 var lengthOfLastWord = function(s) {
     const arr = s.split(' ');
     for (let i = arr.length-1; i >= 0; i--) {
-        console.log(arr[i]);
         if (arr[i]) {
             return arr[i].length
         }
